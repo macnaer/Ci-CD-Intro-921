69c1cb67b86e9035b387e4cca74003675ce5af48
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-09-12 12:51:55 +0300

    Save in file

--- lib/currency.py
+++ lib/currency.py
@@ -1,4 +1,4 @@
-import requests 
+import requests
 if __name__ == "__main__":
     pass
 
@@ -7,4 +7,11 @@ def get_currency(URL):
     responce = requests.get(URL)
     currencies = responce.json()
     for currency in currencies:
-        print(currency['ccy'], " ", currency['base_ccy'], " | ", currency['buy'], " ", currency['sale'])
+        print(currency['ccy'], " ", currency['base_ccy'],
+              " | ", currency['buy'], " ", currency['sale'])
+
+    file = open('currency.txt', 'w')
+    for item in currencies:
+        file.write(item["ccy"] + " " + item["base_ccy"] + " " +
+                   item["buy"] + " | " + item["sale"]+"\n")
+    file.close
